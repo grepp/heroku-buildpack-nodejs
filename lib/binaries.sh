f36f1b65558ef97f7894c0baba2be7f58647a29b
Sangyeol Park <phenolate93@gmail.com>
2019-01-10 15:15:20 +0900

    buildpack이 이미 다운로드된 node binary를 활용하도록 함

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -29,17 +29,9 @@ install_yarn() {
 install_nodejs() {
   local version=${1:-10.x}
   local dir="${2:?}"
+  local number="${NODE_VERSION}"
 
-  echo "Resolving node version $version..."
-  if ! read number url < <(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=$version" "https://nodebin.herokai.com/v1/node/$platform/latest.txt"); then
-    fail_bin_install node $version;
-  fi
-
-  echo "Downloading and installing node $number..."
-  local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
-  if [ "$code" != "200" ]; then
-    echo "Unable to download node: $code" && false
-  fi
+  echo "Pulling and installing node $number..."
   tar xzf /tmp/node.tar.gz -C /tmp
   rm -rf "$dir"/*
   mv /tmp/node-v$number-$os-$cpu/* $dir
