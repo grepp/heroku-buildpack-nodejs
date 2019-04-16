dc30542800e0636a5ddaf35d8c491c0669c1464c
Sangyeol Park <phenolate93@gmail.com>
2019-04-16 14:53:35 +0900

    use local yarn binary

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -1,27 +1,19 @@
 install_yarn() {
   local dir="$1"
-  local version=${2:-1.x}
+  local version=${YARN_VERSION}
   local number
   local url
 
-  echo "Resolving yarn version $version..."
-  if ! read number url < <(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=$version" "https://nodebin.herokai.com/v1/yarn/$platform/latest.txt"); then
-    fail_bin_install yarn $version;
-  fi
-
   echo "Downloading and installing yarn ($number)..."
-  local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/yarn.tar.gz --write-out "%{http_code}")
-  if [ "$code" != "200" ]; then
-    echo "Unable to download yarn: $code" && false
-  fi
   rm -rf $dir
   mkdir -p "$dir"
   # https://github.com/yarnpkg/yarn/issues/770
   if tar --version | grep -q 'gnu'; then
-    tar xzf /tmp/yarn.tar.gz -C "$dir" --strip 1 --warning=no-unknown-keyword
+    tar xzf /tmp/binaries/yarn-v${version}.tar.gz -C "$dir" --strip 1 --warning=no-unknown-keyword
   else
-    tar xzf /tmp/yarn.tar.gz -C "$dir" --strip 1
+    tar xzf /tmp/binaries/yarn-v${version}.tar.gz -C "$dir" --strip 1
   fi
+
   chmod +x $dir/bin/*
   echo "Installed yarn $(yarn --version)"
 }
@@ -32,7 +24,7 @@ install_nodejs() {
   local number="${NODE_VERSION}"
 
   echo "Pulling and installing node $number..."
-  tar xzf /tmp/binaries/node.tar.gz -C /tmp
+  tar xzf /tmp/binaries/node-v$number-$os-$cpu.tar.gz -C /tmp
   rm -rf "$dir"/*
   mv /tmp/node-v$number-$os-$cpu/* $dir
   chmod +x $dir/bin/*
